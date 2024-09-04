23af21d873c1accd4229a6e51d2f8731d8beef71
Wester.Morandi <westerjesesuino@gmail.com>
2024-09-03 22:04:28 -0400

    feat: Update login form and add CSRF protection

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -2,20 +2,19 @@ import User from '#models/user'
 import { HttpContext } from '@adonisjs/core/http'
 
 export default class AuthController {
-  async login({ request, auth, response, session, view }: HttpContext) {
+  async login({ request, auth, response, session }: HttpContext) {
     const { email, password } = request.only(['email', 'password'])
 
     try {
-      // Tenta autenticar o usuário
       const user = await User.verifyCredentials(email, password)
       await auth.use('web').login(user)
 
-      // Redireciona para uma rota protegid
-      return view.render('pages/home', { user })
+      session.flash({ user })
+      return response.redirect('/')
     } catch (error) {
       return response
         .status(400)
-        .send(`<script>alert('Erro ao fazer login'); window.location.href='/register';</script>`)
+        .send(`<script>alert('Erro ao fazer login'); window.location.href='/login';</script>`)
     }
   }
 }
