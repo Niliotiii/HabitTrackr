8d288e39214da87acaad9a3c97d62e7a20dd8b13
Wester.Morandi <westerjesesuino@gmail.com>
2024-09-04 18:50:12 -0400

    add name in meta

--- app/controllers/dashboard_controller.ts
+++ app/controllers/dashboard_controller.ts
@@ -1,11 +1,10 @@
-// app/Controllers/Http/DashboardController.ts
 import ActivityRegister from '#models/activity-register'
 import Goal from '#models/goal'
 import Habit from '#models/habit'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class DashboardController {
-  public async index({ view, auth }: HttpContextContract) {
+  async index({ view, auth }: HttpContextContract) {
     const user = auth.user!
 
     // Contar o número de registros
@@ -15,49 +14,36 @@ export default class DashboardController {
       .where('userId', user.id)
       .count('* as total')
 
-    console.log(habitsCountResult, goalsCountResult, activitiesCountResult)
-
     // Obter o valor da contagem
     const habitsTotal = habitsCountResult[0]?.$extras?.total || 0
     const goalsTotal = goalsCountResult[0]?.$extras?.total || 0
     const activitiesTotal = activitiesCountResult[0]?.$extras?.total || 0
 
-    // Obter os hábitos do usuário e discriminar a proporção de atividades registradas
-    const habits = await Habit.query().where('userId', user.id).preload('category') // Opcional, se você deseja carregar a categoria do hábito
-
-    const habitsDetails = await Promise.all(
-      habits.map(async (habit) => {
-        // Buscar metas relacionadas a cada hábito
-        const goals = await Goal.query().where('habitId', habit.id).where('userId', user.id)
-
-        // Buscar atividades relacionadas a cada meta
-        const goalsDetails = await Promise.all(
-          goals.map(async (goal) => {
-            const activities = await ActivityRegister.query()
-              .where('habitId', habit.id)
-              .where('userId', user.id)
-            return {
-              ...goal.toJSON(),
-              activities,
-            }
-          })
-        )
-
-        return {
-          ...habit.toJSON(),
-          totalGoals: goals.length,
-          completedGoals: goalsDetails.filter((goal) => goal.activities.length > 0).length,
-          goals: goalsDetails,
-        }
+    // Obter as metas, incluindo os hábitos e os registros de atividades relacionados
+    const goals = await Goal.query()
+      .where('userId', user.id)
+      .preload('habit', (habitQuery) => {
+        habitQuery.preload('category')
       })
-    )
+      .preload('activities', (activityQuery) => {
+        activityQuery.where('userId', user.id)
+      })
+
+    // Estruturar os dados de metas, hábitos e atividades
+    const goalsDetails = goals.map((goal) => {
+      return {
+        ...goal.toJSON(),
+        habit: goal.habit.toJSON(),
+        activities: goal.activities.map((activity) => activity.toJSON()),
+      }
+    })
 
     // Renderizar a view com os dados
     return view.render('pages/home', {
       habitsCount: habitsTotal,
       goalsCount: goalsTotal,
       activitiesCount: activitiesTotal,
-      habitsDetails,
+      goalsDetails,
     })
   }
 }
